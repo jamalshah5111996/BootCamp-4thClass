686c756a074f6aabdf9183a52a7e0b17469c9f16
jamalshah5111996 <jamalshah5111996@gmail.com>
2020-12-18 17:22:40 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Dinner from './dinner.js';
+
+function App() {
+  return (
+<div className="firstDiv">
+    <hr/>
+    <Dinner libName="React." Learn="Components," Learn1="Props."/>
+    <hr/>
+    </div>
+  );
+}
+
+export default App;
