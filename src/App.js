17875d4b4e0cd06fb28c326d50c429e963be9d1b
jamalshah5111996 <jamalshah5111996@gmail.com>
2020-12-18 17:29:22 +0500

    changed app.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
 <div className="firstDiv">
     <hr/>
-    <Dinner libName="React." Learn="Components," Learn1="Props."/>
+    <Dinner libName="React." Learn="Components," Learn1="Props." Code="coding."/>
     <hr/>
     </div>
   );
--- src/dinner.js
+++ src/dinner.js
@@ -7,6 +7,7 @@ function Dinner(props){
 <h1>Today we are Learning {props.libName}</h1>
 <h1>We have learned how to make {props.Learn}</h1>
 <h1>We have also learned how to pass {props.Learn1}</h1>
+<h1>Happy {props.Code}</h1>
 </div>
     )
 }
