1dbddee496ad049f20f3fdc4ffe9631cfd295058
jamalshah5111996 <64644606+jamalshah5111996@users.noreply.github.com>
2020-12-19 22:54:06 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import Dinner from './dinner.js';
+import {Message} from './Message.js';
 
 function App() {
+
+let [count,setCount] = useState(0)
+let [isMorning,setMorning] = useState(false)
+
   return (
-<div className="firstDiv">
+<div className={`box ${isMorning ? 'dayLight' : ''}`}>
+<h1>Good {isMorning ? 'Morning' : 'Night'}</h1>
+  <Message counter={count}/>
     <hr/>
-    <Dinner libName="React," Learn="Components," Learn1="Props,"/>
+    <button onClick={()=>setCount(count+1)}>Update Counter</button>
+    <button onClick={()=>setMorning(!isMorning)}>Update Day</button>
     <hr/>
     </div>
   );
