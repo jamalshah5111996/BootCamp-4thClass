002c3badbf7707efb185a81b67d7567907b16177
jamalshah5111996 <jamalshah5111996@gmail.com>
2020-12-19 22:33:06 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
 <div className="firstDiv">
     <hr/>
-    <Dinner libName="React," Learn="Components," Learn1="Props," Code="Coding." />
+    <Dinner libName="React," Learn="Components," Learn1="Props,"/>
     <hr/>
     </div>
   );
